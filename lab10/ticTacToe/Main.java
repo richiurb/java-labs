a2fe6564af98d174348ba41cb845ef75e07f56e4
Rikhard Urbanovich <urihard@mail.ru>
2019-12-02 21:05:34 +0300

    Minor refactoring

--- lab10/ticTacToe/Main.java
+++ lab10/ticTacToe/Main.java
@@ -1,9 +1,13 @@
 package ticTacToe;
 
+import java.util.Scanner;
+
 /**
  * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
  */
 public class Main {
+    private static Scanner in;
+    static int m, n, k;
 
     // Добавить обработку ошибок ввода
 
@@ -15,9 +19,29 @@ public class Main {
 
         int result;
 
+        in = new Scanner(System.in);
+
         //do {
-            result = game.play(new MnkBoard(4, 4, 3));
+            
+            do {
+                System.out.println("Enter \"m\", \"n\", \"k\" for \"m,n,k game\":");
+                m = in.nextInt();
+                n = in.nextInt();
+                k = in.nextInt();
+            } while (!isValidInput());
+
+
+            result = game.play(new MnkBoard(m, n, k));
             System.out.println("Game result: " + result);
         //} while (result != 0);
+    }
+
+    private static boolean isValidInput() {
+        if (k > 2 && m >= k && n >= k) {
+            return true;
+        } else {
+            System.out.println("m, n or k is incorrect for \"m,n,k game\". Please, try again.");
+            return false;
+        }
     }
 }
